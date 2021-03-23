5d6020c7af76b3e571e5d9a0e46de432cc7c454c
Jonadabe Nascimento <jhonndabi.s.n@gmail.com>
2021-03-22 21:30:31 -0300

    First commit

---
+++ ex1/lib/ex1.ex
@@ -0,0 +1,76 @@
+defmodule Ex1 do
+  @moduledoc false
+
+  @doc """
+  "turma de elixir formacao elixir de elixir"
+  %{"turma" => 1, "de" => 2, "elixir" => 3, "formacao" => 1}
+  """
+  def get_words_occurrence(sentence) do
+    String.split(sentence, " ")
+    |> Enum.frequencies()
+  end
+
+  @doc """
+  [1, 5, 0, 8, 10, 9], 19
+
+  [4, 5]
+  """
+  def get_two_sum_index(integers_list, target) do
+    get_two_sum_index(integers_list, integers_list, target)
+  end
+
+  defp get_two_sum_index(original_list, integers_list, target) do
+    [head | tail] = integers_list
+
+    match_sum_value = Enum.find(tail, fn x -> x + head == target end)
+
+    if match_sum_value do
+      first_value = original_list |> get_original_list_index(head)
+      second_value = original_list |> get_original_list_index(match_sum_value)
+
+      [first_value, second_value]
+    else
+      get_two_sum_index(original_list, tail, target)
+    end
+  end
+
+  defp get_original_list_index(original_list, value) do
+    Enum.find_index(original_list, fn x -> x == value end)
+  end
+
+  # @doc """
+  # [1, 5, 0, 8, 10, 9], 19
+
+  # [4, 5]
+  # """
+  # def get_two_sum_index(integers_list, target) do
+  #   [head | tail] = Enum.with_index(integers_list)
+
+  #   get_two_sum_index(head, tail, target)
+  # end
+
+  # defp get_two_sum_index(head, tail, target) do
+  #   match_sum_value = Enum.find(tail, fn x -> elem(x, 0) + elem(head, 0) == target end)
+
+  #   if match_sum_value do
+  #     [elem(head, 1), elem(match_sum_value, 1)]
+  #   else
+  #     [head | tail] = tail
+  #     get_two_sum_index(head, tail, target)
+  #   end
+  # end
+
+  # @doc """
+  # [1, 5, 0, 8, 10, 9], 19
+
+  # [4, 5]
+  # """
+  # def get_two_sum_index(integers_list, target) do
+  #   integers_list
+  #   |> Enum.with_index
+  #   |> Enum.filter(fn {v, _k} ->
+  #     Enum.any?(integers_list, fn x -> x + v == target end)
+  #   end)
+  #   |> Enum.map(fn({_, k}) -> k end)
+  # end
+end
---
+++ ex1/test/ex1_test.exs
@@ -0,0 +1,16 @@
+defmodule Ex1Test do
+  use ExUnit.Case
+  doctest Ex1
+
+  test "count the frenquency of each word in a string" do
+    assert Ex1.get_words_occurrence("turma de elixir formacao elixir de elixir") == %{"turma" => 1, "de" => 2, "elixir" => 3, "formacao" => 1}
+  end
+
+  test "return the index of two elements added together is equal to a target" do
+    assert Ex1.get_two_sum_index([1, 5, 0, 8, 10, 9], 19) == [4, 5]
+  end
+
+  test "return the index of the first two elements added together is equal to a target" do
+    assert Ex1.get_two_sum_index([1, 5, 0, 14, 10, 9], 19) == [1, 3]
+  end
+end
